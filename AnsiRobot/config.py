04859675ad3856af06ae73b463bebc8025c64bdc
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2024-06-13 01:37:35 +0530

    Update config.py

--- AnsiRobot/config.py
+++ AnsiRobot/config.py
@@ -39,14 +39,14 @@ class Config(object):
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "6049338121")
+    DRAGONS = get_user_list("6049338121")
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "6049338121")
+    DEV_USERS = get_user_list("6049338121")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = get_user_list("elevated_users.json", "6049338121")
+    DEMONS = get_user_list("6049338121")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "6049338121")
-    WOLVES = get_user_list("elevated_users.json", "whitelists")
+    TIGERS = get_user_list("6049338121")
+    WOLVES = get_user_list("whitelists")
     DONATION_LINK = "https://t.me/lochakpochak" # EG, paypal
     CERT_PATH = None
     PORT = 5000
